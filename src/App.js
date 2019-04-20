3499d385866185f19007c7c125ea9f800c0a79fa
ultraemme <e.larsson1994@gmail.com>
2019-04-20 19:43:16 +0200

    horizontal & vertical win conditions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <p className="">Player X wins!</p>
         <Grid/>
       </div>
     );
--- src/Grid.js
+++ src/Grid.js
@@ -1,35 +1,64 @@
 import React, { useState, useEffect } from 'react';
 
 const cols = 7, rows = 6;
-const initialGrid = createGrid(cols, rows);
-let turn = 1;
+let turn = 'yellow';
 
-function switchTurn () {
-  turn === 1 ?  turn = 2 : turn = 1;
-  return turn;
-}
-
-function createGrid (cols, rows) {
+//https://stackoverflow.com/questions/9979560/javascript-multidimensional-array-updating-specific-element
+const createGrid = (cols, rows) => {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
     for (let j = 0; j < arr[i].length; j++) {
-      arr[i][j] = 0;
+      arr[i][j] = "";
     }
   }
   return arr;
 }
 
+const switchTurn = () => {
+  turn === 'yellow' ?  turn = 'red' : turn = 'yellow';
+  return turn;
+}
+
 
 const Grid = (props) => {
+  const initialGrid = createGrid(cols, rows);
   const [grid, setGrid] = useState(initialGrid);
 
-  function makeMove(e) {
-    console.log(e.nativeEvent);
+  useEffect(() => {
+    checkWinCondition();
+  }, [grid])
+
+  const checkWinCondition = () => {
+    console.log(grid);
+
+    //vertical
+    for (let col of grid) {
+      for (let i = 0; i < 3; i++) {
+        if (col[i] === col[i+1] && col[i+1] === col[i+2] && col[i+2] === col[i+3] && col[i] !== "") {
+          console.log("vertical winner winner chicken dinner!")
+        }
+      }
+    }
+
+    //horizontal
+    for(let i = 0; i < 4; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] === grid[i+1][j] && grid[i+1][j] === grid[i+2][j] && grid[i+2][j] === grid[i+3][j] && grid[i][j] !== "") {
+          console.log("horizontal winner winner chicken dinner!")
+        }
+      }
+    }
+
+    //diagonal
+
+  }
+
+  const makeMove = (e) => {
     let position = e.target.dataset.position.split("-");
-    let newGrid = JSON.parse(JSON.stringify(grid)); //make and entierly new copy of of an array of arrays to an unknown level of depths
+    let newGrid = JSON.parse(JSON.stringify(grid)); //make and entierly new copy of of an array of arrays with an unknown level of depths
     for (let i = grid[position[0]].length; i >= 0; i--) {
-      if (grid[position[0]][i] === 0) {
+      if (grid[position[0]][i] === "") {
         newGrid[position[0]][i] = switchTurn();
         setGrid(newGrid);
         return;
@@ -45,7 +74,12 @@ const Grid = (props) => {
               <div className="col" key={idx}>
                 {col.map((cell, idx2) => {
                   return (
-                    <div onClick={makeMove} className="cell" data-position={`${idx}-${idx2}`} key={`${idx}-${idx2}`}>{cell}</div>
+                    <div
+                      key={`${idx}-${idx2}`}
+                      className="cell"
+                      style={cell === "" ? null : {backgroundColor: cell}}
+                      onClick={makeMove}
+                      data-position={`${idx}-${idx2}`}/>
                   )})}
               </div>
             )})}
