9eae2f8404a9e2c913b36cc1f94a8fa541101cf7
ultraemme <e.larsson1994@gmail.com>
2019-04-22 11:29:45 +0200

    diagonal win conditions

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
     //vertical
     for (let col of grid) {
       for (let i = 0; i < 3; i++) {
-        if (col[i] === col[i+1] && col[i+1] === col[i+2] && col[i+2] === col[i+3] && col[i] !== "") {
+        if (col[i] === col[i+1] && col[i] === col[i+2] && col[i] === col[i+3] && col[i] !== "") {
           setStatus(`Player ${col[i]} wins!`);
         }
       }
@@ -46,14 +46,28 @@ const App = () => {
     //horizontal
     for(let i = 0; i < 4; i++) {
       for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] === grid[i+1][j] && grid[i+1][j] === grid[i+2][j] && grid[i+2][j] === grid[i+3][j] && grid[i][j] !== "") {
+        if (grid[i][j] === grid[i+1][j] && grid[i][j] === grid[i+2][j] && grid[i][j] === grid[i+3][j] && grid[i][j] !== "") {
           setStatus(`Player ${grid[i][j]} wins!`);
         }
       }
     }
 
     //diagonal
+    for(let i = 0; i < 4; i++) {
+      for(let j = 0; j < 3; j++) {
+        if (grid[i][j] === grid[i+1][j+1] && grid[i][j] === grid[i+2][j+2] && grid[i][j] === grid[i+3][j+3] && grid[i][j] !== "") {
+          console.log("diagonal from top-left to bottom-right hit")
+        }
+      }
+    }
 
+    for(let i = 0; i < 4; i++) {
+      for(let j = 3; j < 6; j++) {
+        if (grid[i][j] === grid[i+1][j-1] && grid[i][j] === grid[i+2][j-2] && grid[i][j] === grid[i+3][j-3] && grid[i][j] !== "") {
+          console.log("diagonal from bottom-left to top-right hit")
+        }
+      }
+    }
   }
 
   const resetGame = () => {
