14f29125b5799ea7fee04dcb0d7caae8fd2ad487
ultraemme <e.larsson1994@gmail.com>
2019-04-23 11:35:41 +0200

    draw, should work on any grid size

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Grid from './components/Grid';
 
 const cols = 7, rows = 6;
 let turn = 'yellow';
+let count = 0;
 
 const switchPlayer = () => {
   turn === 'yellow' ?  turn = 'red' : turn = 'yellow';
   return turn;
 }
-
-//https://stackoverflow.com/questions/9979560/javascript-multidimensional-array-updating-specific-element
 const createGrid = (cols, rows) => {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
@@ -29,60 +28,75 @@ const App = () => {
 
   useEffect(() => {
     checkWinCondition();
+    checkDrawCondition();
   }, [grid])
 
   const checkWinCondition = () => {
     console.log(grid);
 
     //vertical
-    for (let col of grid) {
-      for (let i = 0; i < 3; i++) {
-        if (col[i] === col[i+1] && col[i] === col[i+2] && col[i] === col[i+3] && col[i] !== "") {
-          setStatus(`Player ${col[i]} wins!`);
-        }
-      }
-    }
-
-    //horizontal
-    for(let i = 0; i < 4; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] === grid[i+1][j] && grid[i][j] === grid[i+2][j] && grid[i][j] === grid[i+3][j] && grid[i][j] !== "") {
+    for(let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length-3; j++) {
+        if (grid[i][j] === grid[i][j+1] && grid[i][j] === grid[i][j+2] && grid[i][j] === grid[i][j+3] && grid[i][j] !== "") {
+          console.log('vertical win');
           setStatus(`Player ${grid[i][j]} wins!`);
         }
       }
     }
 
-    //diagonal
-    for(let i = 0; i < 4; i++) {
-      for(let j = 0; j < 3; j++) {
-        if (grid[i][j] === grid[i+1][j+1] && grid[i][j] === grid[i+2][j+2] && grid[i][j] === grid[i+3][j+3] && grid[i][j] !== "") {
-          console.log("diagonal from top-left to bottom-right hit")
+    //horizontal
+    for(let i = 0; i < grid.length-3; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] === grid[i+1][j] && grid[i][j] === grid[i+2][j] && grid[i][j] === grid[i+3][j] && grid[i][j] !== "") {
+          console.log('horizontal win');
+          setStatus(`Player ${grid[i][j]} wins!`);
         }
       }
     }
 
-    for(let i = 0; i < 4; i++) {
-      for(let j = 3; j < 6; j++) {
+    //diagonals
+    for(let i = 0; i < grid.length-3; i++) {
+      for(let j = 0; j < grid[i].length-3; j++) {
+        if (grid[i][j] === grid[i+1][j+1] && grid[i][j] === grid[i+2][j+2] && grid[i][j] === grid[i+3][j+3] && grid[i][j] !== "") {
+          console.log("diagonal from top-left to bottom-right hit")
+          setStatus(`Player ${grid[i][j]} wins!`);
+        }
+      }
+    }
+
+    for(let i = 0; i < grid.length-3; i++) {
+      for(let j = 0; j < grid[i].length; j++) {
         if (grid[i][j] === grid[i+1][j-1] && grid[i][j] === grid[i+2][j-2] && grid[i][j] === grid[i+3][j-3] && grid[i][j] !== "") {
           console.log("diagonal from bottom-left to top-right hit")
+          setStatus(`Player ${grid[i][j]} wins!`);
         }
       }
     }
   }
 
+  const checkDrawCondition = () => {
+    if(count >= cols*rows && !status) {
+      setStatus("Game draw!");
+    }
+  }
+
   const resetGame = () => {
+    count = 0;
     setGrid(createGrid(cols, rows));
     setStatus("");
   }
 
   const makeMove = (e) => {
-    let pos = e.target.dataset.pos.split("-");
-    let newGrid = JSON.parse(JSON.stringify(grid)); //make and entierly new copy of of an array of arrays with an unknown level of depths
-    for (let i = grid[pos[0]].length; i >= 0; i--) {
-      if (grid[pos[0]][i] === "") {
-        newGrid[pos[0]][i] = switchPlayer();
-        setGrid(newGrid);
-        return;
+    if (!status) {
+      let pos = e.target.dataset.pos.split("-");
+      let newGrid = JSON.parse(JSON.stringify(grid));
+      for (let i = grid[pos[0]].length; i >= 0; i--) {
+        if (grid[pos[0]][i] === "") {
+          count++;
+          newGrid[pos[0]][i] = switchPlayer();
+          setGrid(newGrid);
+          return;
+        }
       }
     }
   }
@@ -90,9 +104,7 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={resetGame} className="btn__reset">RESET</button>
-      {status &&
-        <p className="msg__status">{status}</p>
-      }
+      <p className="msg__status">{status}</p>
       <Grid grid={grid} makeMove={makeMove}/>
     </div>
   );
